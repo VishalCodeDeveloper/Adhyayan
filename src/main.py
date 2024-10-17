5702923d53cc8b576896b7a1169208ec2bd0b6f5
Vishal Bhuyar <34850559+VishalCodeDeveloper@users.noreply.github.com>
2024-10-17 14:36:48 +0530

    Add files via upload
    Displaying results changed.

--- src/main.py
+++ src/main.py
@@ -35,41 +35,12 @@ def upload_file():
     # Get audio duration and transcribe
     audio_duration = get_audio_duration(audio_file)
     transcribed_text = transcribe_audio(audio_file)
-
-    # Analyze text and video
-    print(f"\n\n")
-    print(f"transcribed_text : ", transcribed_text)
-    print(f"\n\n")
-    text_analysis = analyze_text(transcribed_text, audio_duration)
-
-    # print("text_analysis.sentiment : ", text_analysis.sentiment)
-
-    for keyword in text_analysis.items():
-            print(keyword)
-            print("\n\n\n")
-
-    video_analysis = analyze_video(video_path)
-
-    print(f"Emotion changes: {video_analysis}")
-
-    for time, emotion in video_analysis.emotion_changes.items():
-         print(f"Time: {time} - Emotion: {emotion}")
-
-    for time, detected in video_analysis.eye_contact.items():
-        print(f"Time: {time} - Eye Contact: {'Yes' if detected else 'No'}")
-         
     
-    for second, gestures in video_analysis.hand_gestures.items():
-        print(f"Second: {second} - Gestures")
-        for gesture, count in gestures.items():
-            print(f"{gesture}: {count}")
-
-    results(transcribed_text, text_analysis, video_analysis)
-    # return jsonify({
-    #     "transcribed_text": transcribed_text,
-    #     "text_analysis": text_analysis,
-    #     "video_analysis": video_analysis
-    # })
+    text_analysis = analyze_text(transcribed_text, audio_duration)
+    for name, value in text_analysis.items():
+        print(f"{name}: {value}")
+    video_analysis = analyze_video(video_path)
+    return results(transcribed_text, text_analysis, video_analysis)
 
 def results(transcribed_text, text_analysis, video_analysis):
     # Simulated analysis data    
